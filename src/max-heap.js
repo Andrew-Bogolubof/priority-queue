d841649f3984c8cd5a3db9b3dce09048f0e13b3c
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-03-10 19:31:33 +0300

    feat: add insert node, without second test

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		const addNode = new Node(data, priority);
+		this.insertNode(addNode);
+		this.shiftNodeUp(addNode);
 	}
 
 	pop() {
@@ -22,11 +25,14 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		return this.parentNodes.lenght;
 	}
 
 	isEmpty() {
-		
+		if (this.root === null && this.parentNodes.length == 0) {
+			return true;
+		}
+		return false;
 	}
 
 	clear() {
@@ -34,7 +40,15 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if (this.isEmpty()) {
+			this.root = node;
+			this.parentNodes.push(node);
+			return;
+		}
+		this.parentNodes.push(node);
+		let length = this.parentNodes.length;
+		let parent = Math.floor( (length - 2) / 2 );
+		this.parentNodes[parent].appendChild(node);
 	}
 
 	shiftNodeUp(node) {
